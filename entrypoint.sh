79f8ce62c9ad09e4ff3e6618b7a2dfb8cd3ca685
guci <guci314@gmail.com>
2020-02-25 10:30:38 +0800

    df

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /brook
 # wget -O brook https://github.com/txthinking/brook/releases/download/v${VERSION}/brook_linux_arm64
-wget -O brook https://github.com/txthinking/brook/releases/download/v${VERSION}/brook
+wget -O brook https://github.com/txthinking/brook/releases/download/v20200201/brook
 
 chmod +x /brook/brook
 
